d59dd369d6d90196a1c8bf962472b2ca20a52cd6
Eunji <dmswl5959@naver.com>
2024-11-14 14:33:20 +0900

    [fix] Schedule Entity 수정중

--- travelbuddy-backend/src/main/java/travelbuddy/function/schedule/entity/Schedule.java
+++ travelbuddy-backend/src/main/java/travelbuddy/function/schedule/entity/Schedule.java
@@ -2,6 +2,7 @@ package travelbuddy.function.schedule.entity;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.Cascade;
+import travelbuddy.function.member.entity.Account;
 import travelbuddy.function.member.entity.MemberAnswer;
 
 import java.sql.Time;
@@ -11,38 +12,15 @@ import java.util.Date;
 @Table(name="tbl_schedule")
 public class Schedule {
 
-    @Id
-    @Column(name="sche_code")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int scheCode;
-
-    @ManyToOne
-    @JoinColumn(name="region_code")
-    private Region region;
-
-//    private Accom accom;
-
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name="member_answer_code")
-    private MemberAnswer memberAnswer;
-
-    @Column(name="sche_list")
-    private String scheList;
-
-    @Column(name="sche_start_date")
-    private String scheStartDate;
-
-    @Column(name="sche_end_date")
-    private String scheEndDate;
-
-    @Column(name="travel_time")
-    private String travelTime;
-
-    @Column(name="sche_time")
-    private String scheTime;
-
-    public Schedule() {
-    }
-
+    int scheCode;
+    Region region;
+    Accommodation accommodation;
+    Account account;
+    MemberAnswer memberAnswer;
+    String scheList;
+    String scheStartDate;
+    String scheEndDate;
+    String travelTime;
+    String scheTime;
 
 }

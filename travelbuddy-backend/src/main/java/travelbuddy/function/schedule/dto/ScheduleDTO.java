0f3cdd94291c6d357284d9e31ff7369db4aff1a1
Eunji <dmswl5959@naver.com>
2024-11-14 14:38:33 +0900

    [fix] Schedule DTO 수정완료

--- travelbuddy-backend/src/main/java/travelbuddy/function/schedule/dto/ScheduleDTO.java
+++ travelbuddy-backend/src/main/java/travelbuddy/function/schedule/dto/ScheduleDTO.java
@@ -7,25 +7,31 @@ public class ScheduleDTO {
 
     private int scheCode;
     private int regionCode;
-    private int memberAnswerCode;
     private int accomCode;
+    private int memberCode;
+    private int memberAnswerCode;
     private String scheList;
     private String scheStartDate;
     private String scheEndDate;
+    private String scheStartTime;
+    private String scheEndTime;
     private String travelTime;
     private String scheTime;
 
     public ScheduleDTO() {
     }
 
-    public ScheduleDTO(int scheCode, int regionCode, int memberAnswerCode, int accomCode, String scheList, String scheStartDate, String scheEndDate, String travelTime, String scheTime) {
+    public ScheduleDTO(int scheCode, int regionCode, int accomCode, int memberCode, int memberAnswerCode, String scheList, String scheStartDate, String scheEndDate, String scheStartTime, String scheEndTime, String travelTime, String scheTime) {
         this.scheCode = scheCode;
         this.regionCode = regionCode;
-        this.memberAnswerCode = memberAnswerCode;
         this.accomCode = accomCode;
+        this.memberCode = memberCode;
+        this.memberAnswerCode = memberAnswerCode;
         this.scheList = scheList;
         this.scheStartDate = scheStartDate;
         this.scheEndDate = scheEndDate;
+        this.scheStartTime = scheStartTime;
+        this.scheEndTime = scheEndTime;
         this.travelTime = travelTime;
         this.scheTime = scheTime;
     }
@@ -46,20 +52,28 @@ public class ScheduleDTO {
         this.regionCode = regionCode;
     }
 
-    public int getMemberAnswerCode() {
-        return memberAnswerCode;
-    }
-
-    public void setMemberAnswerCode(int memberAnswerCode) {
-        this.memberAnswerCode = memberAnswerCode;
-    }
-
     public int getAccomCode() {
         return accomCode;
     }
 
     public void setAccomCode(int accomCode) {
         this.accomCode = accomCode;
+    }
+
+    public int getMemberCode() {
+        return memberCode;
+    }
+
+    public void setMemberCode(int memberCode) {
+        this.memberCode = memberCode;
+    }
+
+    public int getMemberAnswerCode() {
+        return memberAnswerCode;
+    }
+
+    public void setMemberAnswerCode(int memberAnswerCode) {
+        this.memberAnswerCode = memberAnswerCode;
     }
 
     public String getScheList() {
@@ -86,6 +100,22 @@ public class ScheduleDTO {
         this.scheEndDate = scheEndDate;
     }
 
+    public String getScheStartTime() {
+        return scheStartTime;
+    }
+
+    public void setScheStartTime(String scheStartTime) {
+        this.scheStartTime = scheStartTime;
+    }
+
+    public String getScheEndTime() {
+        return scheEndTime;
+    }
+
+    public void setScheEndTime(String scheEndTime) {
+        this.scheEndTime = scheEndTime;
+    }
+
     public String getTravelTime() {
         return travelTime;
     }
@@ -107,11 +137,14 @@ public class ScheduleDTO {
         return "ScheduleDTO{" +
                 "scheCode=" + scheCode +
                 ", regionCode=" + regionCode +
-                ", memberAnswerCode=" + memberAnswerCode +
                 ", accomCode=" + accomCode +
+                ", memberCode=" + memberCode +
+                ", memberAnswerCode=" + memberAnswerCode +
                 ", scheList='" + scheList + '\'' +
                 ", scheStartDate='" + scheStartDate + '\'' +
                 ", scheEndDate='" + scheEndDate + '\'' +
+                ", scheStartTime='" + scheStartTime + '\'' +
+                ", scheEndTime='" + scheEndTime + '\'' +
                 ", travelTime='" + travelTime + '\'' +
                 ", scheTime='" + scheTime + '\'' +
                 '}';

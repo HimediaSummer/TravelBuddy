a8566a6d065af760ed2aec04294490a224146245
Eunji <dmswl5959@naver.com>
2024-11-14 14:45:13 +0900

    [fix] Schedule Entity 수정완료

--- travelbuddy-backend/src/main/java/travelbuddy/function/schedule/entity/Schedule.java
+++ travelbuddy-backend/src/main/java/travelbuddy/function/schedule/entity/Schedule.java
@@ -12,15 +12,46 @@ import java.util.Date;
 @Table(name="tbl_schedule")
 public class Schedule {
 
-    int scheCode;
-    Region region;
-    Accommodation accommodation;
-    Account account;
-    MemberAnswer memberAnswer;
-    String scheList;
-    String scheStartDate;
-    String scheEndDate;
-    String travelTime;
-    String scheTime;
+    @Id
+    @Column(name="sche_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int scheCode;
+
+    @ManyToOne
+    @JoinColumn(name="region_code")
+    private Region region;
+
+    @ManyToOne
+    @JoinColumn(name="accom_code")
+    private Accommodation accommodation;
+
+    @ManyToOne
+    @JoinColumn(name="member_code")
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name="member_answer_code")
+    private MemberAnswer memberAnswer;
+
+    @Column(name="sche_list")
+    private String scheList;
+
+    @Column(name="sche_start_date")
+    private String scheStartDate;
+
+    @Column(name="sche_end_date")
+    private String scheEndDate;
+
+    @Column(name="sche_start_time")
+    private String scheStartTime;
+
+    @Column(name="sche_end_time")
+    private String scheEndTime;
+
+    @Column(name="travel_time")
+    private String travelTime;
+
+    @Column(name="sche_time")
+    private String scheTime;
 
 }

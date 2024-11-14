1620027d0b5a94688e54c84ee4ba6e339c0038b7
summerchoco <shkkk90@daum.net>
2024-11-14 09:39:39 +0900

    [feat] react 화면 구성

--- travelbuddy-backend/src/main/java/travelbuddy/function/member/repository/MypageRepository.java
+++ travelbuddy-backend/src/main/java/travelbuddy/function/member/repository/MypageRepository.java
@@ -3,6 +3,7 @@ package travelbuddy.function.member.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import travelbuddy.function.community.buddy.entity.Buddy;
 import travelbuddy.function.member.entity.AccountEntity;
 import travelbuddy.function.member.entity.MemberBuddyData;
@@ -11,7 +12,9 @@ import java.util.List;
 
 public interface MypageRepository extends JpaRepository<MemberBuddyData, Integer> {
 
+    @Query("SELECT m FROM MemberBuddyData m WHERE m.memberCode = ?1")
     List<MemberBuddyData> findByMemberCode(int memberCode);
 
-    Page<MemberBuddyData> findByMemberCode(int i, Pageable paging);
+    @Query("SELECT m FROM MemberBuddyData m WHERE m.memberCode = ?1")
+    Page<MemberBuddyData> findByMemberCode(int memberCode, Pageable paging);
 }
--- travelbuddy-frontend/src/components/products/Main.js
+++ travelbuddy-frontend/src/components/products/Main.js
@@ -14,6 +14,34 @@ function App() {
   return (
     <div className="App">
       <h1>{message}</h1>
+      날짜<input/><input/>
+      <br/>
+      여행장소
+      <select name="selectedFruit">
+        <option value="apple">제주도</option>
+        <option value="banana">대전</option>
+        <option value="orange">부산</option>
+      </select>
+      <br/>
+      숙소
+      <select name="selectedFruit">
+        <option value="orange">아파트</option>
+        <option value="banana">빌라</option>
+        <option value="apple">호텔/모텔</option>
+      </select>
+      <br/>
+      질문 : 새로운 모임의 단톡방이 만들어 졌을 때 나는?
+      <br/>
+      <button>인사만 하고 별다른 톡은 남기지 않는다.</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button>완전 소꿉친구 바이브 나온다</button>
+      <br/>
+      <button>어색한 분위기를 살리려고 인사하고, 리액션이나 이모티콘을 사용해본다.</button>
+      <button>우우 유령이다 우우</button>
+      <br/>
+      <button>일정생성</button>
     </div>
   );
 }

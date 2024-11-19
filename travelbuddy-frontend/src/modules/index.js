c2c3a05f7c60e13754c2e49067e5d846ab9503ff
KimHoDong <tunys123@gmail.com>
2024-11-19 21:05:58 +0900

    [feat] 리액트 작성중

--- travelbuddy-frontend/src/modules/index.js
+++ travelbuddy-frontend/src/modules/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 import mypageBuddyReducer from './MypageBuddyModule.js';
 import memberReducer from './MemberModule.js'
 import qnaReducer from './QnaModule.js';
+import faqReducer from './FaqModule.js';
+
 
 const rootReducer = combineReducers({
 	mypageBuddyReducer,
 	memberReducer,
-	qnaReducer
+	qnaReducer,
+	faqReducer
 });
 
 export default rootReducer;

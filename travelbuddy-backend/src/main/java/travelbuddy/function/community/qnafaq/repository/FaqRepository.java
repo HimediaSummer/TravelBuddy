8fe0dd6de6aa664c0c7363594ab5b81903e1237d
KimHoDong <tunys123@gmail.com>
2024-11-19 16:16:46 +0900

    [feat] Faq 회원 전체조회,상세조회 기능 추가

---
+++ travelbuddy-backend/src/main/java/travelbuddy/function/community/qnafaq/repository/FaqRepository.java
@@ -0,0 +1,8 @@
+package travelbuddy.function.community.qnafaq.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import travelbuddy.function.community.qnafaq.entity.Faq;
+
+public interface FaqRepository extends JpaRepository<Faq,Integer> {
+
+}

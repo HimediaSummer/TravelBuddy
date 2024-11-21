63036f3175b83968d70516bd9705d69f4bb6b264
summerchoco <shkkk90@daum.net>
2024-11-21 12:51:53 +0900

    [feat] api key 로직 구현중

--- travelbuddy-frontend/src/components/products/Schedule/SummarySchedule.js
+++ travelbuddy-frontend/src/components/products/Schedule/SummarySchedule.js
@@ -11,6 +11,7 @@ function SummarySchedule({ travelData }) {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
+					Authorization: `Bearer ${process.env.REACT_APP_GPT_API_KEY}`
 				},
 				body: JSON.stringify(travelData), // travelData는 날짜, 숙소, 지역, 질문지 정보를 포함해야 함
 			});

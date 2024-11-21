e9cbc2a6757319366d17aae6e8bbc82691980de9
Eunji <dmswl5959@naver.com>
2024-11-21 10:32:14 +0900

    [feat] 지도 API 수정중...

--- travelbuddy-frontend/src/components/products/Schedule.js
+++ travelbuddy-frontend/src/components/products/Schedule.js
@@ -9,12 +9,14 @@ import Summary from './Schedule/SummarySchedule';
 
 function Schedule() {
 	const [currentStep, setCurrentStep] = useState(0); // z컴포넌트 바꾸기?
+	// 지역값 전달하기 도전중
+	const [selectedRegion, setSelectedRegion] = useState(null);
 
 	// 각 단계 컴포넌트들
 	const steps = [
 		<DateSchedule onNext={() => setCurrentStep(1)} />,
-		<RegionSchedule onNext={() => setCurrentStep(2)} />,
-		<AccomSchedule onNext={() => setCurrentStep(3)} />,
+		<RegionSchedule onNext={() => setCurrentStep(2)} selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion}/>,
+		<AccomSchedule onNext={() => setCurrentStep(3)}  selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion}/>,
 		<QuestionSchedule onNext={() => setCurrentStep(4)} />,
 		<Summary />
 	];

4287b4baef77e08a46eb657f7e1074ea3df1f437
yejin4069 <jinny4069@gmail.com>
2024-12-03 10:23:39 +0900

    [style] MyMatchDetail.css 작업중

--- travelbuddy-frontend/src/pages/member/mypage/MyMatchDetail.js
+++ travelbuddy-frontend/src/pages/member/mypage/MyMatchDetail.js
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { callMyMatchDetailsAPI, cancelMatchAPI } from '../../../apis/MypageAPICalls';
 import { useParams } from 'react-router-dom';
-// import './MyMatchDetail.css';
+import './MyMatchDetail.css';
 
 function MyMatchDetail() {
     const navigate = useNavigate();
@@ -27,42 +27,117 @@ function MyMatchDetail() {
     };
 
     return (
-        <div>
-            <h3>내가 신청한 글 상세조회</h3>
+        <div className="page-container">
+            <h3 className="buddy-title">MY신청</h3>
             {buddyList.length > 0 ? (
-                <ul>
-                    {buddyList.map((buddy, index) => (
-                        <li key={index}>
-                            <p>제목 : {buddy.buddyTitle}</p>
-                            <p>내용 : {buddy.buddyContents}</p>
-                            {/* <p>이미지 : {buddy.buddyImg}</p> */}
-                            <p>지역 : {buddy.region.regionName}</p>
-                            <p>작성일자 : {buddy.buddyCreate}</p>
-                            <p>작성자 : {buddy.account.memberName}</p>
-                            <p>버디유형 : {buddy.buddyType.buddyTypeName}</p>
-                            <ul>
-                                {buddyMatchDataList
-                                    .filter((match) => match.buddyCode === buddy.buddyCode)
-                                    .map((match, matchIndex) => (
-                                        <li key={matchIndex}>
-                                            <p>ApplyId: {match.applyId}</p>
-                                            <p>
-                                                ApplyStatus:{' '}
-                                                {match.applyStatus === 1
-                                                    ? '대기중'
-                                                    : match.applyStatus === 2
-                                                    ? '수락'
-                                                    : '거절'}
-                                            </p>
-                                            <button onClick={() => handleCancel(match.buddyMatchCode)}>
-                                                신청취소
-                                            </button>
-                                        </li>
-                                    ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
+                <table className="main-table">
+                <thead>
+                    <tr>
+                        <td>제목</td>
+                        <td>
+                            <div className="buddyDetail-title">
+                                <p>
+                                    {buddyList[0].buddyTitle &&
+                                    buddyList[0].buddyTitle.length > 20
+                                        ? `${buddyList[0].buddyTitle.slice(0, 20)}...`
+                                        : buddyList[0].buddyTitle}
+                                </p>
+                            </div>
+                        </td>
+                        <td>버디 유형</td>
+                        <td>
+                            <div className="buddy-typeName">
+                                <p>{buddyList[0].buddyType?.buddyTypeName || "버디 유형 없음"}</p>
+                            </div>
+                        </td>
+                        <td>지역</td>
+                        <td>
+                            <div className="buddy-regionName">
+                                <p>{buddyList[0].region?.regionName || "지역 없음"}</p>
+                            </div>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="left-cell" colSpan="6">
+                            <div className="buddy-img">
+                                {buddyList[0].buddyImg ? (
+                                    <img
+                                        src={buddyList[0].buddyImg}
+                                        alt="Buddy Image"
+                                    />
+                                ) : null}
+                            </div>
+                            <div className="buddy-contents">
+                                <p>{buddyList[0].buddyContents}</p>
+                            </div>
+                            {/* ApplyId 및 ApplyStatus */}
+                            <div className="match-info">
+                                    <ul>
+                                        {buddyMatchDataList
+                                            .filter((match) => match.buddyCode === buddyList[0].buddyCode)
+                                            .map((match, matchIndex) => (
+                                                <li key={matchIndex}>
+                                                    <p>
+                                                        <span className="label">ApplyId:</span>{" "}
+                                                        {match.applyId}
+                                                    </p>
+                                                    <p>
+                                                        <span className="label">ApplyStatus:</span>{" "}
+                                                        {match.applyStatus === 1
+                                                            ? "대기중"
+                                                            : match.applyStatus === 2
+                                                            ? "수락"
+                                                            : "거절"}
+                                                    </p>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                </div>
+                            <button
+                                className="cancel-button"
+                                onClick={() => handleCancel(buddyList[0].buddyCode)}
+                            >
+                                신청 취소
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+                // <ul>
+                //     {buddyList.map((buddy, index) => (
+                //         <li key={index}>
+                //             <p>제목 : {buddy.buddyTitle}</p>
+                //             <p>내용 : {buddy.buddyContents}</p>
+                //             {/* <p>이미지 : {buddy.buddyImg}</p> */}
+                //             <p>지역 : {buddy.region.regionName}</p>
+                //             <p>작성일자 : {buddy.buddyCreate}</p>
+                //             <p>작성자 : {buddy.account.memberName}</p>
+                //             <p>버디유형 : {buddy.buddyType.buddyTypeName}</p>
+                //             <ul>
+                //                 {buddyMatchDataList
+                //                     .filter((match) => match.buddyCode === buddy.buddyCode)
+                //                     .map((match, matchIndex) => (
+                //                         <li key={matchIndex}>
+                //                             <p>ApplyId: {match.applyId}</p>
+                //                             <p>
+                //                                 ApplyStatus:{' '}
+                //                                 {match.applyStatus === 1
+                //                                     ? '대기중'
+                //                                     : match.applyStatus === 2
+                //                                     ? '수락'
+                //                                     : '거절'}
+                //                             </p>
+                //                             <button onClick={() => handleCancel(match.buddyMatchCode)}>
+                //                                 신청취소
+                //                             </button>
+                //                         </li>
+                //                     ))}
+                //             </ul>
+                //         </li>
+                //     ))}
+                // </ul>
             ) : (
                 <p>신청한 버디가 없습니다.</p>
             )}
